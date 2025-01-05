9794d7b71361729e3927c3c38edfd52290801b1c
MSI\hosiu <hosiuchuen05@gmail.com>
2025-01-05 14:00:45 +0800

    request mapping and param test

---
+++ springmvc/springmvc_request_mapping/src/main/java/com/schh/config/ServletContainersInitConfig.java
@@ -0,0 +1,24 @@
+package com.schh.config;
+
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class ServletContainersInitConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ springmvc/springmvc_request_mapping/src/main/java/com/schh/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.schh.controller;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/save")
+    @ResponseBody
+    public String save() {
+        System.out.println("user save....");
+        return "{'info': 'user save'}";
+    }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        System.out.println("user delete....");
+        return "{'info': 'user delete'}";
+    }
+}
