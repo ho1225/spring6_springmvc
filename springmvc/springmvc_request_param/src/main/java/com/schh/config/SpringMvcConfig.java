a64ee6c427e54267aac235f4d87119f2aa181c82
MSI\hosiu <hosiuchuen05@gmail.com>
2025-01-08 20:51:50 +0800

    add json and date param request

--- springmvc/springmvc_request_param/src/main/java/com/schh/config/SpringMvcConfig.java
+++ springmvc/springmvc_request_param/src/main/java/com/schh/config/SpringMvcConfig.java
@@ -2,8 +2,10 @@ package com.schh.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.schh.controller")
+@EnableWebMvc
 public class SpringMvcConfig {
 }
--- springmvc/springmvc_request_param/src/main/java/com/schh/controller/UserController.java
+++ springmvc/springmvc_request_param/src/main/java/com/schh/controller/UserController.java
@@ -2,12 +2,15 @@ package com.schh.controller;
 
 import com.schh.domain.User;
 import com.sun.org.apache.xpath.internal.objects.XString;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -16,8 +19,8 @@ public class UserController {
     @RequestMapping("/commonParam")
     @ResponseBody
     public String commonParam(String name, int age) {
-        System.out.println("common param transfer name => "+ name);
-        System.out.println("common param transfer age => "+ age);
+        System.out.println("common param transfer name => " + name);
+        System.out.println("common param transfer age => " + age);
         return "{'module': 'common param'}";
     }
 
@@ -25,8 +28,8 @@ public class UserController {
     @RequestMapping("/commonParamDifferentName")
     @ResponseBody
     public String commonParamDifferentName(@RequestParam("name") String userName, int age) {
-        System.out.println("common param transfer name => "+ userName);
-        System.out.println("common param transfer age => "+ age);
+        System.out.println("common param transfer name => " + userName);
+        System.out.println("common param transfer age => " + age);
         return "{'module': 'common param'}";
     }
 
@@ -59,4 +62,35 @@ public class UserController {
         return "{'module': 'common param'}";
     }
 
+    @RequestMapping("/listParamForJson")
+    @ResponseBody
+    public String listParamForJson(@RequestBody List<String> likes) {
+        System.out.println("list common(json) list => " + likes);
+        return "{'module': 'list common for json param'}";
+    }
+
+    @RequestMapping("/pojoParamForJson")
+    @ResponseBody
+    public String pojoParamForJson(@RequestBody User user) {
+        System.out.println("list pojo(json) => " + user);
+        return "{'module': 'list pojo for json param'}";
+    }
+
+    @RequestMapping("/listPojoParamForJson")
+    @ResponseBody
+    public String listPojoParamForJson(@RequestBody List<User> list) {
+        System.out.println("list pojo(json) list => " + list);
+        return "{'module': 'list pojo for json param'}";
+    }
+
+    @RequestMapping("/dateParam")
+    @ResponseBody
+    public String dateParam(Date date,
+                            @DateTimeFormat(pattern = "yyyy-MM-dd") Date date1,
+                            @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss") Date date2) {
+        System.out.println("dateParam => " + date);
+        System.out.println("dateParam => " + date1);
+        System.out.println("dateParam => " + date2);
+        return "{'module': 'date Param'}";
+    }
 }
