db5306f8f82c57a471cc0a194e93a838876a104a
MSI\hosiu <hosiuchuen05@gmail.com>
2025-01-27 21:26:00 +0800

    finish ssm and create new module for creating different REST result

---
+++ springmvc/springmvc_result/src/main/java/com/schh/service/impl/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package com.schh.service.impl;
+
+import com.schh.dao.BookDao;
+import com.schh.domain.Book;
+import com.schh.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean save(Book book) {
+        bookDao.save(book);
+        return true;
+    }
+
+    @Override
+    public boolean update(Book book) {
+        bookDao.update(book);
+        return true;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        bookDao.delete(id);
+        return true;
+    }
+
+    @Override
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
