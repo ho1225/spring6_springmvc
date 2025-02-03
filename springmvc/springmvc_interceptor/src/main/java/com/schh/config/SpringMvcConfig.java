62093cd197fec308db70a51f0f6fbfba25547ade
MSI\hosiu <hosiuchuen05@gmail.com>
2025-02-03 20:19:25 +0800

    test interceptor chain

--- springmvc/springmvc_interceptor/src/main/java/com/schh/config/SpringMvcConfig.java
+++ springmvc/springmvc_interceptor/src/main/java/com/schh/config/SpringMvcConfig.java
@@ -1,11 +1,27 @@
 package com.schh.config;
 
+import com.schh.controller.interceptor.ProjectInterceptor;
+import com.schh.controller.interceptor.ProjectInterceptor2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan({"com.schh.controller", "com.schh.config"})
+@ComponentScan("com.schh.controller")
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+    @Autowired
+    private ProjectInterceptor2 projectInterceptor2;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books/**");
+        registry.addInterceptor(projectInterceptor2).addPathPatterns("/books/**");
+    }
 }
--- springmvc/springmvc_interceptor/src/main/java/com/schh/controller/interceptor/ProjectInterceptor2.java
+++ springmvc/springmvc_interceptor/src/main/java/com/schh/controller/interceptor/ProjectInterceptor2.java
@@ -9,24 +9,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class ProjectInterceptor implements HandlerInterceptor {
+public class ProjectInterceptor2 implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String contentType = request.getHeader("Content-Type");
-        HandlerMethod hm = (HandlerMethod) handler;
-        System.out.println(hm.getMethod().getName());
-        System.out.println("ProjectInterceptor preHandle"+contentType);
+//        String contentType = request.getHeader("Content-Type");
+//        HandlerMethod hm = (HandlerMethod) handler;
+//        System.out.println(hm.getMethod().getName());
+        System.out.println("ProjectInterceptor preHandle2222  ");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
-        System.out.println("ProjectInterceptor postHandle");
+        System.out.println("ProjectInterceptor postHandle2222  ");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("ProjectInterceptor afterCompletion");
+        System.out.println("ProjectInterceptor afterCompletion2222");
     }
 }
